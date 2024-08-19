6725e6b906f6609cdf30821ff4e78cbda4b2200f
Tasha <nataliiamelnykova9@gmail.com>
2024-08-19 12:11:02 +0200

    icons

--- movie-browser/src/components/BottomNav.jsx
+++ movie-browser/src/components/BottomNav.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './BottomNav.css';
+import HomeIcon from './HomeIcon.jsx';
+import SearchIcon from './SearchIcon.jsx';
+import AccountIcon from './AccountIcon.jsx';
 
 function BottomNav() {
   return (
     <nav className="bottom-nav">
-      <Link to="/"><img src="./assets/img/home.svg" alt="" /></Link>
-      <Link to="/discover">Discover</Link>
-      <Link to="/account">Account</Link>
+      <Link to="/"><HomeIcon /></Link>
+      <Link to="/discover"><SearchIcon /></Link>
+      <Link to="/account"><AccountIcon /></Link>
     </nav>
   );
 }
