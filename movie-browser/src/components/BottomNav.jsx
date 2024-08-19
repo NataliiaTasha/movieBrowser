af15c6e95960d74eb22540618aa618eed34202af
Tasha <nataliiamelnykova9@gmail.com>
2024-08-19 13:54:31 +0200

    icons active/inactive

--- movie-browser/src/components/BottomNav.jsx
+++ movie-browser/src/components/BottomNav.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './BottomNav.css';
 import HomeIcon from './HomeIcon.jsx';
 import SearchIcon from './SearchIcon.jsx';
 import AccountIcon from './AccountIcon.jsx';
 
 function BottomNav() {
+  const location = useLocation(); //get the current path
   return (
     <nav className="bottom-nav">
-      <Link to="/"><HomeIcon /></Link>
-      <Link to="/discover"><SearchIcon /></Link>
-      <Link to="/account"><AccountIcon /></Link>
+      <Link to="/">
+      <HomeIcon isActive={location.pathname === '/'} />
+      </Link>
+      <Link to="/discover">
+      <SearchIcon isActive={location.pathname === '/discover'} />
+      </Link>
+      <Link to="/account">
+      <AccountIcon  isActive={location.pathname === '/account'}/>
+      </Link>
     </nav>
   );
 }
